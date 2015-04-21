69fbc92864bab48f0a762e71cf125151a39fefc7
Alexandre Aubin <alexandre.aubin@cern.ch>
2015-04-21 15:53:22 +0200

    Done first version of theory chapter + change in thesis style

--- media/theory/fermionMasses.py
+++ media/theory/fermionMasses.py
@@ -4,19 +4,19 @@ import matplotlib
 #matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-X = [0.8, 1, 1.2, 1.8, 2, 2.2, 2.8, 3, 3.2]
-Y = [0.511, 4.8, 2.4, 105.7, 104, 1270, 1777, 4200, 171200 ]
-labels = [ "$e$", "$d$", "$u$", r"$\mu$", "$s$", "$c$", r"$\tau$", r"$b$", r"$t$" ]
+X        = [0.8,   1,     1.2,   1.8,      2,     2.2,   2.8,       3,      3.2,    3.8,    4,      4.2   ]
+labels   = ["$e$", "$d$", "$u$", r"$\mu$", "$s$", "$c$", r"$\tau$", r"$b$", r"$t$", "$W$",  "$Z$",  "$h$" ]
+Y        = [0.511, 4.8,   2.4,   105.7,    95,    1270,  1777,      4200,   173500, 80385,  91187,  125000]
+YerrUp   = [0,     0.7,   0.7,   0,        5,     0,     0,         0,      1,      0,      0,      0     ]
+YerrDown = [0,     0.5,   0.3,   0,        5,     0,     0,         0,      1,      0,      0,      0     ]
 
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 
-
 plt.subplots_adjust(bottom = 0.1)
 
-plt.scatter(
-        X, Y, marker = 'o', c = '#0077ff', s = 70, alpha=0.8,
-        cmap = plt.get_cmap('Spectral'))
+plt.errorbar(
+        X, Y, yerr=[YerrUp,YerrDown], fmt = 'o', c = '#0099ff')
 
 for label, x, y in zip(labels, X, Y):
     plt.annotate(
@@ -25,12 +25,12 @@ for label, x, y in zip(labels, X, Y):
             textcoords = 'offset points', ha = 'center', va = 'bottom',
             bbox = dict(boxstyle = 'round,pad=0.5', alpha = 0.0), fontsize=20)
 
-xAxis = [1, 2, 3]
-xAxisLabels = ["1$^{st}$ gen", "2$^{nd}$ gen", "3$^{rd}$ gen"]
+xAxis = [1, 2, 3, 4]
+xAxisLabels = ["1$^{st}$ gen", "2$^{nd}$ gen", "3$^{rd}$ gen", "Bosons"]
 plt.xticks(xAxis, xAxisLabels)
 plt.yscale("log")
-plt.ylabel("Mass [GeV]", fontsize=20)
+plt.ylabel("Mass [MeV]", fontsize=20)
 plt.tick_params(axis='both', which='major', labelsize=20)
-#plt.show()
+
 plt.savefig("fermionMasses.pdf")
 
